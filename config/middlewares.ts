b9678ab0b1563b3b9fd4e064d970148b34d11fd6
unknown <torsten@isleofbyte.de>
2025-05-17 17:03:58 +0200

    CORS erweitert für Bildzugriff

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,14 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['*'], // Oder z. B. ['http://localhost:5500'] für Live-Server
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
@@ -10,3 +17,4 @@ export default [
   'strapi::favicon',
   'strapi::public',
 ];
+
