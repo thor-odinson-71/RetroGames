06a184686201639d3819ef9ddbd1b771c41f36cd
unknown <torsten@isleofbyte.de>
2025-05-17 17:31:33 +0200

    Fix: CORS für localhost explizit freigegeben

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,14 +2,14 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['http://localhost:5500', 'http://localhost:3000', 'http://localhost'], // alle lokalen Varianten
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-    },
-  },
+  // {
+  //   name: 'strapi::cors',
+  //   config: {
+  //     origin: ['http://localhost:5500', 'http://localhost:3000', 'http://localhost'], // alle lokalen Varianten
+  //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  //     headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+  //   },
+  // },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
